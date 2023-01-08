7f29f28ce6ce422074f30a730ea37cc8f6881115
Jahaidul Islam <63470340+jahaidul@users.noreply.github.com>
2023-01-08 16:33:47 +0600

    Create sWAPcASE.py

---
+++ sWAPcASE.py
@@ -0,0 +1,16 @@
+def swap_case(sentence):
+    updated_s = ""
+    for c in sentence:
+        if c.isupper():
+            updated_s += c.lower()
+        elif c.islower():
+            updated_s += c.upper()
+        else:
+            updated_s += c
+    return updated_s
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
